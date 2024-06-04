5d3cd7f5235c39cca9aff251c246bae8a8b515d6
David Ruiz <david.a.ruiz@unl.edu.ec>
2024-06-03 22:56:32 -0500

    Update and rename 18._ContarNprimofor to 18._ContarNprimowhile.c

--- 18._ContarNprimowhile.c
+++ 18._ContarNprimowhile.c
@@ -10,12 +10,15 @@ int esPrimo(int num) {
 
 int main() {
     int n, count = 0;
+    int i = 2;  // Inicializamos i en 2 porque el 1 no es primo
 
     printf("Introduce un número: ");
     scanf("%d", &n);
 
-    for (int i = 2; i <= n; i++) {
+    // Usamos while en lugar de for para contar los números primos
+    while (i <= n) {
         if (esPrimo(i)) count++;
+        i++;
     }
 
     printf("Hay %d números primos hasta %d\n", count, n);
